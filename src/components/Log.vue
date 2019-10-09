3b80b076b46f9df43d81b407c0324d09aab72312
Rinat <vereshaginrin@gmail.com>
2019-10-09 17:02:17 +0300

    Test_2

---
+++ src/components/Log.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- <p> HI </p> -->
+<!--    <p>{{hi}}</p>-->
+
+    <input type="text" v-model="message">
+<!--    <p v-text="message"></p>-->
+</template>
+
+
+<script>
+export default {
+    name: "Log",
+    data: () => ({
+        hi: 'sayFUCK',
+        message:'',
+    })   
+}
+</script>
